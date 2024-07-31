8e8f56ec726c6bdfdb20572505741b8da96b9c80
muntasirul <muntasirul.msd@gmail.com>
2024-07-31 18:43:12 +0530

    backend url updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
 proxy:{
-  "/admin":"http://localhost:4000"
+  "/admin":"https://foody-backend-k0z8.onrender.com"
 }
   },
   plugins: [react()],
